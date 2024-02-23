b0ec0c932c337515448319915adb3a5c5bb46776
Rubens Lima <rubenslima@gmail.com>
2024-02-23 17:33:18 -0300

    final do dia

--- README.md
+++ README.md
@@ -8,3 +8,4 @@ https://www.youtube.com/watch?v=rcMuTUpqUsU
 https://www.youtube.com/watch?v=6Tj8_iKqh_k
 https://www.youtube.com/watch?v=Hj09tFCdjSw
 https://www.youtube.com/watch?v=IZvJOlE5lgc
+https://phylos.net/2023-07-10/python-com-flet
---
+++ models.py
@@ -0,0 +1,20 @@
+from sqlalchemy import create_engine, Column, Integer, String,  Float
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+CONN = "sqlite:///produtos.db"
+
+engine = create_engine(CONN, echo=True)
+Session = sessionmaker(bind=engine)
+session = Session()
+Base = declarative_base()
+
+
+class Produto(Base):
+    __tablename__ = "Produto"
+    id = Column(Integer, primary_key=True)
+    titulo = Column(String(50))
+    preco = Column(Float())
+
+    
+Base.metadata.create_all(engine)
